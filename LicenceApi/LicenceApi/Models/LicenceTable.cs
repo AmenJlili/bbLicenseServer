1e97b6a858013e6d8df3738d505258a94ba6353f
Akash <akashptn10@gmail.com>
2021-08-26 21:10:55 +0530

    Web Api 2.0
    Submit Web Api and SQL database

---
+++ LicenceApi/LicenceApi/Models/LicenceTable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LicenceApi.Models
+{
+    public class LicenceTable
+    {
+
+        public int Id { get; set; }
+        public string Licensekey { get; set; }
+        public string ExpirationDate { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerEmail { get; set; }
+        public bool IsDelete { get; set; }
+        public string DeleteDate { get; set; }
+    }
+}
