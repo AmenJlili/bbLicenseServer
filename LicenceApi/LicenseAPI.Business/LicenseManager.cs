c86241efa51b2ec0427c301ab8affef0cabc47d7
Amen Jlili (Home) <amen@bluebyte.biz>
2021-09-14 23:28:16 -0700

    Add business logic

---
+++ LicenceApi/LicenseAPI.Business/LicenseManager.cs
@@ -0,0 +1,43 @@
+﻿using Standard.Licensing;
+using System;
+using System.IO;
+using System.Text;
+
+namespace LicenseAPI.Business
+{
+    public class LicenseManager
+    {
+        public string PrivateKey { get; set; }
+        public string PassPhrase { get; set; }
+
+        public string PublicKey { get; set; }
+        public LicenseManager()
+        {
+             
+        }
+
+        public void New(bbLicense bblicense, string passPhrase, string licensePath)
+        {
+            var license = License.New()
+    .WithUniqueIdentifier(Guid.NewGuid())
+    .As(LicenseType.Standard)
+    .ExpiresAt(bblicense.Expiry)
+    .LicensedTo($"{bblicense.CustomerName} {bblicense.CustomerLastName}", bblicense.CustomerEmail)
+    .CreateAndSignWithPrivateKey(PrivateKey, passPhrase);
+
+        File.WriteAllText(licensePath, license.ToString(), Encoding.UTF8);
+        }
+    }
+
+    public class bbLicense
+    {
+        public string CustomerName { get; set; }
+        public string CustomerLastName { get; set; }
+     
+        public string CustomerEmail { get; set; }
+
+        public DateTime Expiry { get; set; }
+    }
+
+
+}
---
+++ LicenceApi/LicenseAPI.PairGenerator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LicenseAPI.PairGenerator
+{
+    class Program
+    {
+        /*
+         Pass    : Blue Byte Systems, Inc.
+         Private : MHcwIwYKKoZIhvcNAQwBAzAVBBBicgsQh07gerhOiRUYUPnDAgEKBFB1vfxoYkihhwUDTZJ0UGJ86320WsTIVFyRLngD151KbN5kKffFTCMrGsRKo3kl3zvT1lZI+nLFMAxYwqzfJQsyqkEZwv4J3KKoIk6UeqqgeA==
+         Public  : MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEwZq2xT4eBZQVgNATKNs8KwwCtscuv2QsiBiunMBZ/CRinlh7aluD1WfZodEHQdpv1oLTnBzLxDbL+I2dAqr/pQ==
+         */
+
+        static void Main(string[] args)
+        {
+            var passPhrase = "Blue Byte Systems, Inc.";
+            var keyGenerator = Standard.Licensing.Security.Cryptography.KeyGenerator.Create();
+            var keyPair = keyGenerator.GenerateKeyPair();
+            var privateKey = keyPair.ToEncryptedPrivateKeyString(passPhrase);
+            var publicKey = keyPair.ToPublicKeyString();
+
+            Console.WriteLine($"Pass    : {passPhrase}");
+            Console.WriteLine($"Private : {privateKey}");
+            Console.WriteLine($"Public  : {publicKey}");
+
+
+            Console.Read();
+        }
+    }
+}
